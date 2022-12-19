fdce3696dfe7694124b5fdb30032fc184917ff8a
pocketclint <agileclint@gmail.com>
2022-12-19 15:04:29 -0600

    added functioning question array and refined scoreboard

--- assets/js/scoreboard.js
+++ assets/js/scoreboard.js
@@ -3,9 +3,13 @@ var clearBtn = document.querySelector("#clear");
 function scoreboard() {
     var highscores = JSON.parse(window.localStorage.getItem("highscores")) || [];
 
+    highscores.sort(function(a, b) {
+      return b.score - a.score;
+    });
+
     highscores.forEach(function(score) {
         var liEl = document.createElement("li");
-        liEl.textContent = score.initials
+        liEl.textContent = score.initials + " Score: " + score.score;
         var olEl = document.getElementById("highscores");
         olEl.appendChild(liEl);
     });
